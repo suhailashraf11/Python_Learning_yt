1d5211fbdbacc24d2b1a7ff5461412d42c9d26e7
suhail ashraf <suhailashraf282@gmail.com>
2025-10-23 11:57:06 +0530

    change

--- day2.py
+++ day2.py
@@ -1,3 +1,14 @@
 # video 42 and if else and series
 
+age =    int(input(" enter you nam: "))
+
+
+if age >= 18:
+    print('you are above 18')
+    print(age)
+
+else:
+
+    print("you are below 18 ")
+    print("you ages is ", age)
 
