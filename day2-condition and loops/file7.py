35b3c593dd28beb503eb2270641ff932abf64bad
suhail ashraf <suhailashraf282@gmail.com>
2025-10-24 15:42:54 +0530

    new excercise

--- day2-condition and loops/file7.py
+++ day2-condition and loops/file7.py
@@ -1,1 +1,17 @@
-print('hello world')
+# using if elif else
+
+
+
+age = int(input("Enter your age: "))
+
+if age < 3:
+    print("Ticket is free")
+elif 3 <= age <= 10:
+    print("Ticket price is 150")
+elif 11 <= age <= 60:
+    print("Ticket price is 250")
+else:
+    print("It’s not safe for you!")
+
+
+
