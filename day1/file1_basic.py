79b04a01f75a91146acf994d67518df5f9c5d92d
suhail ashraf <suhailashraf282@gmail.com>
2025-10-24 16:03:12 +0530

    basic add

---
+++ day1/file1_basic.py
@@ -0,0 +1,43 @@
+print('hello world')
+
+
+intgers  = 23
+strings  = "hello suahil"
+
+print( intgers, strings)
+
+a,b,c = str(3),int(3), float(3)
+print('Type of data string', a)
+print('Type of data intger ', b)
+print('Type of data float ', c)
+
+
+# how to check data type
+
+check_data_type  = 'suahil'
+check_data_type2 = 23.4
+check_data_type3 = 1
+print(type(check_data_type))
+print(type(check_data_type2))
+print(type(check_data_type3))
+
+
+#many values to multiple variables at time
+o ,b ,c = 'orange', 'banana', 'charry'
+print(o,b,c)
+
+
+# one value to multiple variable
+x = y = z =  'all same\n'
+print(x,y,z)
+
+
+# unplack a collection such as  list tuple
+
+car = ['skoda', 'audi', 'rangce rover']
+c,a,r = car
+print('Unpacking list items',c)
+print('Unpacking list items',a)
+print('Unpacking list items',r)
+
+
